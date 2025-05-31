d0d7cefe5791d7b1cc56cd87d2531d528213dc1f
yangFenTuoZi <140885477+yangFenTuoZi@users.noreply.github.com>
2025-05-31 17:45:52 +0800

    update

--- src/main/java/stand/ronghui/HttpRequest.java
+++ src/main/java/stand/ronghui/HttpRequest.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.net.URL;
 import java.net.HttpURLConnection;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.Map;
 import java.util.List;
 import java.io.PrintWriter;
@@ -21,7 +22,7 @@ public class HttpRequest {
 
 	public static HttpRequest get(String url, String[][] headers) {
 		HttpRequest request = new HttpRequest();
-		String result = "";
+		StringBuilder result = new StringBuilder();
 		BufferedReader in = null;
 		try {
 			URL realUrl = new URL(url);
@@ -33,12 +34,12 @@ public class HttpRequest {
 			request.cookies = getCookies(connection);
 			//获取网页内容
 			if (connection.getResponseCode() == HttpURLConnection.HTTP_OK) {
-				in = new BufferedReader(new InputStreamReader(connection.getInputStream(), "UTF-8"));
+				in = new BufferedReader(new InputStreamReader(connection.getInputStream(), StandardCharsets.UTF_8));
 				String line;
 				while ((line = in.readLine()) != null) {
-					result += line + "\n";
+					result.append(line).append("\n");
 				}
-				request.text = result;
+				request.text = result.toString();
 			}
 		} catch (Exception e) {
 			System.out.println("发送GET请求出现异常！" + e);
@@ -58,24 +59,24 @@ public class HttpRequest {
 		HttpRequest request = new HttpRequest();
 		PrintWriter out = null;
 		BufferedReader in = null;
-		String result = "";
+		StringBuilder result = new StringBuilder();
 		try {
 			URL realUrl = new URL(url);
 			HttpURLConnection conn = (HttpURLConnection) realUrl.openConnection();
 			buildHeader(conn, headers);
 			conn.setDoOutput(true);
 			conn.setDoInput(true);
-			out = new PrintWriter(new OutputStreamWriter(conn.getOutputStream(), "UTF-8"));
+			out = new PrintWriter(new OutputStreamWriter(conn.getOutputStream(), StandardCharsets.UTF_8));
 			out.print(data);
 			out.flush();
 			request.cookies = getCookies(conn);
 			if (conn.getResponseCode() == HttpURLConnection.HTTP_OK) {
-				in = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
+				in = new BufferedReader(new InputStreamReader(conn.getInputStream(), StandardCharsets.UTF_8));
 				String line;
 				while ((line = in.readLine()) != null) {
-					result += line;
+					result.append(line);
 				}
-				request.text = result;
+				request.text = result.toString();
 			}
 		} catch (Exception e) {
 			System.out.println("发送 POST 请求出现异常！" + e);
@@ -95,11 +96,11 @@ public class HttpRequest {
 	}
 
 	private static void buildHeader(HttpURLConnection connection, String[][] headers) {
-		for (int i = 0;i < headers.length;i++) {
-			for (int j = 0;j < headers[i].length;j += 2) {
-				connection.setRequestProperty(headers[i][j], headers[i][j + 1]);
-			}
-		}
+        for (String[] header : headers) {
+            for (int j = 0; j < header.length; j += 2) {
+                connection.setRequestProperty(header[j], header[j + 1]);
+            }
+        }
 	}
 
 	private static String buildParams(String url, String[][] parms) {
@@ -107,24 +108,22 @@ public class HttpRequest {
 		sb.append("?");
 		for (int i = 0;i < parms.length;i++) {
 			for (int j = 0;j < parms[i].length;j += 2) {
-				sb.append(parms[i][j] + "=" + parms[i][j + 1]);
+				sb.append(parms[i][j]).append("=").append(parms[i][j + 1]);
 			}
-			if (i != parms.length) {
-				sb.append("&");
-			}
-		}
+            sb.append("&");
+        }
 		return sb.toString();
 	}
 
 	private static String getCookies(HttpURLConnection connection) {
 		Map<String, List<String>> map = connection.getHeaderFields();
 		List<String> cookiesList=map.get("Set-Cookie");
-		String newcookie="";
+		StringBuilder newcookie= new StringBuilder();
 		for (String cook : cookiesList) {
-			newcookie += cook.split(";")[0] + "; ";
+			newcookie.append(cook.split(";")[0]).append("; ");
 		}
-		if (newcookie.length() != 0) {
-			return newcookie;
+		if (newcookie.length() > 0) {
+			return newcookie.toString();
 		}
 		return "";
 	}
